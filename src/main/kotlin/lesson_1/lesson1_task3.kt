d6a1a80fc478e3922ceca0d4b89018d75dd20194
MikhailReshetnikov <resh75@list.ru>
2025-02-15 23:55:54 +0200

    KSL-1-3 time space flight

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val yearOfFirstFlightInSpace = 1961
+    var hour = 9
+    var minute = 7
+
+
+    println(yearOfFirstFlightInSpace)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    print(hour)
+    print(":")
+    print(minute)
+
+}
