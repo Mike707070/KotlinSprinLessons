d34830258fe00d3832de4f834bff4ce0b154636e
MikhailReshetnikov <resh75@list.ru>
2025-02-15 21:31:10 +0200

    KSL-1-1 creat логика экрана для ИМ

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val numberOfOrders = 75
+    val textAfterPurchase = "Thank you, next purchase 10% discount"
+
+    println(numberOfOrders)
+    println(textAfterPurchase)
+
+}
