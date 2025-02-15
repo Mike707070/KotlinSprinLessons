bd6a855c079190d4b65215b044270c6f3d1a51a7
MikhailReshetnikov <resh75@list.ru>
2025-02-15 22:37:37 +0200

    KSL-1-2 creat логика экрана для ИМ заказы и кол-во сотрудников

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val numberOfOrders = 75
+    val textAfterPurchase = "Thank you, next purchase 10% discount"
+
+    println(numberOfOrders)
+    println(textAfterPurchase)
+
+    var numberOfEmployees = 2000
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+}
