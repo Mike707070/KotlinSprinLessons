640cae0576c2737e5d010574a5b2228d8ee46132
MikhailReshetnikov <resh75@list.ru>
2025-02-17 21:15:49 +0200

    KLS-1-3 исправлен формат и объявлены строки

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,15 +1,16 @@
 package org.example.lesson_1
 
+import javax.swing.Spring
+
 fun main() {
 
     val yearOfFirstFlightInSpace = 1961
-    var hourOfFirstFlightInSpace: Int = 9
-    var minuteOfFirstFlightInSpace: Int = 7
-
+    var hourOfFirstFlightInSpace: String = "09"
+    var minuteOfFirstFlightInSpace: String = "07"
 
     println("Год полета в космос $yearOfFirstFlightInSpace")
-    println("Час полета в космос 0$hourOfFirstFlightInSpace")
-    println("Минута полета в космос 0$minuteOfFirstFlightInSpace")
+    println("Час полета в космос $hourOfFirstFlightInSpace")
+    println("Минута полета в космос $minuteOfFirstFlightInSpace")
 
     var hourOfReturn = 10
     var minuteOfReturn = 55
