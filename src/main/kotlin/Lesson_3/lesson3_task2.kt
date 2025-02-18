f8cd1acf96fea3eb3e293555abc09e96f0016502
MikhailReshetnikov <resh75@list.ru>
2025-02-18 20:50:12 +0200

    KS-3-1 создано утреннее и ночное приветствие

---
+++ src/main/kotlin/Lesson_3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_3
+
+fun main() {
+
+    val ageAt20 = 20
+    val ageAt22 = 22
+
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+
+    var lastName: String
+    lastName = "Андреева"
+    println("$firstName $middleName $lastName, $ageAt20")
+    lastName = "Сидорова"
+    println("$firstName $middleName $lastName, $ageAt22")
+}
