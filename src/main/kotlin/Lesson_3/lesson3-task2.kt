e30e27a8ec69969d50f0d263bf1328730e9e67b1
MikhailReshetnikov <resh75@list.ru>
2025-02-22 13:55:30 +0200

    KS-3-2 изменение переменных возраста и оптимизация

---
+++ src/main/kotlin/Lesson_3/lesson3-task2.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_3
+
+fun main() {
+
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    var lastName = "Сидорова"
+    var age = 20
+    println("$firstName $middleName $lastName, $age")
+
+    age += 2
+    lastName = "Андреева"
+    println("$firstName $middleName $lastName, $age")
+
+
+}
